0999fb9cb881260d854a6228d32fc0c92a7be5cc
merazzak <info.merazzak@gmail.com>
2025-05-24 00:04:08 +0600

    first commit

---
+++ src/routes/router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Gemini Clone</div>,
+  },
+]);
+
+export default router;
