7a31fd47928d98b9f16eb09a7a786b670b12cd4c
merazzak <info.merazzak@gmail.com>
2025-06-09 11:43:47 +0600

    add main layout

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,14 +9,33 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
 
-      <div className="grrd">
-        <p>
-          <span>Hello, Dev.</span>
-        </p>
-        <p>How can I help you?</p>
-      </div>
+      <div className="main-container max-w-[900px] mx-auto">
+        <div className="greed">
+          <p>
+            <span>Hello, Dev.</span>
+          </p>
+          <p>How can I help you?</p>
+        </div>
 
-      <div className="cards"></div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summerize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstrom team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
